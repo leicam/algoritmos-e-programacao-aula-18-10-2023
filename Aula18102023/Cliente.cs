21382633391ffbf271f6b4920d0e798a88ce5cb2
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2023-10-18 20:27:30 -0300

    feat: sistema de cadastro de clientes

---
+++ Aula18102023/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula18102023
+{
+    // a classe que irá representar um cliente
+    internal class Cliente
+    {
+        // as variaveis representam atributos para o nosso cliente
+        private string _nome;
+        private string _sobrenome;
+        private string _cpf;
+
+        // o método construtor obriga a passagem de parâmetros obrigatórios
+        public Cliente(string nome, string sobrenome, string cpf)
+        {
+            _nome = nome;
+            _sobrenome = sobrenome;
+            _cpf = cpf;
+        }
+
+        //método sobreescrito, principio de encapsulamento
+        public override string ToString()
+        {
+            return $"Nome: {_nome} | Sobrenome: {_sobrenome} | CPF: {_cpf}";
+        }
+    }
+}
---
+++ Aula18102023/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Aula18102023
+{
+    internal class Program
+    {
+        private static List<Cliente> _clientes = new List<Cliente>();
+
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+
+            // do - while é uma estrutura de repetição
+            do
+            {
+                Console.WriteLine("**MENU**");
+                Console.WriteLine("1 - Cadastrar Cliente");
+                Console.WriteLine("2 - Listar Clientes");
+                Console.WriteLine("0 - Sair");
+
+                opcao = int.Parse(Console.ReadLine());
+
+                // estrutura de condição
+                switch (opcao)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        CadastrarCliente();
+                        break;
+                    case 2:
+                        ListarClientes();
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida! Verifique.");
+                        break;
+                }
+
+            } while (opcao != 0);
+        }
+
+        private static void CadastrarCliente()
+        {
+            Console.WriteLine("Informe o nome do cliente:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Informe o sobrenome do cliente:");
+            string sobrenome = Console.ReadLine();
+
+            Console.WriteLine("Informe o CPF do cliente:");
+            string cpf = Console.ReadLine();
+
+            Cliente cliente = new Cliente(nome, sobrenome, cpf);
+            _clientes.Add(cliente);
+        }
+
+        private static void ListarClientes()
+        {
+            foreach(Cliente cliente in _clientes)
+            {
+                Console.WriteLine(cliente.ToString());
+            }
+        }
+    }
+}
